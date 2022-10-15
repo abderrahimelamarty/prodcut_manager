0cd4f21b653fc1a2054b9b650ff8532258d8b40c
Abderrahim El-amarty <abderrahimelamarty396@gmail.com>
2022-10-15 13:28:08 +0100

    abd

---
+++ src/main/java/web/ControleurServlet.java
@@ -0,0 +1,26 @@
+package web;
+
+import java.io.IOException;
+
+import dao.IProduitDao;
+import dao.ProduitDaoImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class ControleurServlet extends HttpServlet {
+	private IProduitDao metier;
+	@Override
+	public void init() throws ServletException {
+		 metier =new ProduitDaoImpl();
+		 
+	}
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse responce) throws ServletException, IOException {
+		request.getRequestDispatcher("produits.jsp").forward(request, responce);
+	}
+	
+
+}
---
+++ src/main/webapp/produits.jsp
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<!-- CSS only -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<title>Produits</title>
+</head>
+<body>
+<br>
+<br>
+<div class="container mt-5">
+<div class="panel panel-primary " >
+<div class="panel-heading text-center"> Recherche des produits</div>
+<div class="panel-body">
+<form action="chercher.do" method="get">
+<label> Mot Clé</label>
+<input type="text" name="motCle">
+<button type="submit">chercher</button>
+</form>
+</div>
+</div>
+
+
+</div>
+</body>
+</html>
